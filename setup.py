fc94d207c2514e7959a3a54e2b0e8ca5d73594d4
Jonathan kossick <dev.kossick@gmail.com>
2017-04-19 23:14:46 +0200

    new setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='Voc2brain',
+      version='4.9',
+      description='Voc2brain is a simple flashcard software',
+      author='Jonathan Kossick',
+      author_email='dev.kossick@gmail.com',
+      #url='https://www.python.org/sigs/distutils-sig/',
+      packages=['distutils', 'distutils.command'],
+     )
