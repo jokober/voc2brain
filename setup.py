5fbc3ae4ec1526b9cbce4f1746f2f2ce42b69cd4
Jonathan kossick <dev.kossick@gmail.com>
2017-05-18 23:34:02 +0200

    update setup.py

--- setup.py
+++ setup.py
@@ -3,14 +3,29 @@
 from setuptools import setup, find_packages
 
 setup(name='Voc2brain',
-      version='4.9',
+      version='4.9a',
       description='Voc2brain is a simple flashcard software',
       author='Jonathan Kossick',
       author_email='dev.kossick@gmail.com',
       url='https://github.com/jokober/voc2brain',
 
+      classifiers=[
+                  "Development Status :: 3 - Alpha",
+                  #"Development Status :: 4 - Beta",
+                  #"Development Status :: 5 - Production/Stable",
+                  "Environment :: X11 Applications :: Qt",
+                  "Intended Audience :: Education",
+                  "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
+                  "Programming Language :: Python :: 2.7",
+                  "Topic :: Education"
+             ],
+
       install_requires=[
-            'setuptools'
+            'setuptools',
+            'python-qt5'
+            'numpy',
+            'SQLAlchemy'
+
       ],
 
       packages=find_packages("."),
@@ -22,3 +37,5 @@ setup(name='Voc2brain',
 
       }
      )
+
+
